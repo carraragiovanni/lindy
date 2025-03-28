c750b3a9f576d0467f93b9e193d8880a96ed86f0
Giovanni Carrara <carrara.g@gmail.com>
2025-03-28 14:25:06 -0700

    Lindy update

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-Default JS content
+function getRandomColor(){return'#'+Math.floor(Math.random()*16777215).toString(16).padStart(6,'0');}const faces=document.querySelectorAll('.face');setInterval(()=>{faces.forEach(f=>{f.style.background=getRandomColor();});},1000);let angleX=0,angleY=0;function animate(){angleX+=0.5;angleY+=0.5;document.getElementById('cube').style.transform=`rotateX(${angleX}deg) rotateY(${angleY}deg)`;requestAnimationFrame(animate);}animate();
